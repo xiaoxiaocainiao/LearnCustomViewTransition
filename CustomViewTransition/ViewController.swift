e99ccdec7eba952984fadf5b7bd6096a2ddce442
michael wang <wangyan.work@qq.com>
2016-10-18 12:26:55 +0800

    Initial Commit

---
+++ CustomViewTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomViewTransition
+//
+//  Created by 王雁 on 2016/10/18.
+//  Copyright © 2016年 michael wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
