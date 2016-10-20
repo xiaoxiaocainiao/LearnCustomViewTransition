cbc9bacf7b24158c49a0caab69565763e99299fb
michael wang <wangyan.work@qq.com>
2016-10-20 16:31:28 +0800

    bugfixed: the interactive of present animation emample is not work

--- CustomViewTransition/CustomPresentAnimationWithInteractiveViewController.swift
+++ CustomViewTransition/CustomPresentAnimationWithInteractiveViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class CustomPresentAnimationWithInteractiveViewController: UIViewController, UIViewControllerTransitioningDelegate {
     
     private let _presentAnimation = CustomPresentAnimation()
-    private let _customPresentInteractiveTransition = CustomPresentInteractiveTransition()
+    private var _customPresentInteractiveTransition = CustomPresentInteractiveTransition()
+    private let _viewController = PresentedViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,10 @@ class CustomPresentAnimationWithInteractiveViewController: UIViewController, UIV
         btn.addTarget(self, action: #selector(showPresent), for: UIControlEvents.touchUpInside)
         btn.backgroundColor = UIColor.white
         view.addSubview(btn)
+        
+        
+        _customPresentInteractiveTransition.attachToViewController(fromViewController: self, toViewController: _viewController)
+        _viewController.transitioningDelegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,15 +39,17 @@ class CustomPresentAnimationWithInteractiveViewController: UIViewController, UIV
     //MARK: - private logic
     
     func showPresent() {
-        let viewController = PresentedViewController()
-        _customPresentInteractiveTransition.attachToViewController(fromViewController: self, toViewController: viewController)
-        viewController.transitioningDelegate = self
-        self.present(viewController, animated: true, completion: nil)
+        self.present(_viewController, animated: true, completion: nil)
     }
     
     //MARK: - UIViewControllerTransitioningDelegate
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return _presentAnimation
+    }
+    
+    
+    func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return _customPresentInteractiveTransition.transitionInProgress() ? _customPresentInteractiveTransition : nil
     }
     
 }
